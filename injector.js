0d61f9eee460f6c14ba3cb68317d76fbb97eb2a5
3ventic <admin@3v.fi>
2016-11-16 17:28:06 +0200

    add version selection

--- injector.js
+++ injector.js
@@ -1,18 +1,35 @@
 var cid = '';
-function getCid() {
+var acceptHeader = 'application/vnd.twitchtv.3+json';
+function getOptions() {
     chrome.storage.sync.get({
-        cid: ''
+        cid: '',
+		version: 3
     }, function (data) {
         cid = data.cid;
+		acceptHeader = 'application/vnd.twitchtv.' + data.version + '+json';
     });
 }
-getCid();
+getOptions();
 chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
-    getCid();
+    getOptions();
+	var replaced = false;
+	for (var i = 0; i < details.requestHeaders.length; ++i) {
+		if (details.requestHeaders[i].name === 'Accept') {
+			details.requestHeaders[i].value = acceptHeader;
+			replaced = true;
+			break;
+		}
+	}
     details.requestHeaders.push({
         name: 'Client-ID',
         value: cid
     });
+	if (!replaced) {
+		details.requestHeaders.push({
+			name: 'Accept',
+			value: acceptHeader
+		});
+	}
     return { requestHeaders: details.requestHeaders };
 }, {
     urls: [
--- options.js
+++ options.js
@@ -1,14 +1,17 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     chrome.storage.sync.get({
-        cid: ''
+        cid: '',
+		version: 3
     }, function (items) {
         document.getElementById('cid').value = items.cid;
+        document.getElementById('version').value = items.version;
     });
 });
 document.getElementById('save').addEventListener('click', function () {
     chrome.storage.sync.set({
-        cid: document.getElementById('cid').value
+        cid: document.getElementById('cid').value,
+		version: document.getElementById('version').value
     }, function () {
         var status = document.getElementById('status');
         status.textContent = 'Saved';
