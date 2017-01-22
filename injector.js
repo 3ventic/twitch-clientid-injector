156df5c42e0d83d3d4a75b5277fcd1830a085386
3v <admin@3v.fi>
2017-01-22 16:23:55 +0200

    add oauth token, default version and fix delayed apply for setting changes

--- injector.js
+++ injector.js
@@ -1,12 +1,19 @@
 var cid = '';
-var acceptHeader = 'application/vnd.twitchtv.v3+json';
+var acceptHeader = 'application/json';
+var token = '';
 function getOptions() {
     chrome.storage.sync.get({
         cid: '',
-		version: 3
+		version: 0,
+        token: ''
     }, function (data) {
         cid = data.cid;
-		acceptHeader = 'application/vnd.twitchtv.v' + data.version + '+json';
+        if (data.version > 0) {
+		    acceptHeader = 'application/vnd.twitchtv.v' + data.version + '+json';
+        } else {
+            acceptHeader = 'application/json';
+        }
+        token = data.token;
     });
 }
 getOptions();
@@ -20,16 +27,24 @@ chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
 			break;
 		}
 	}
-    details.requestHeaders.push({
-        name: 'Client-ID',
-        value: cid
-    });
+    if (cid.length > 0) {
+        details.requestHeaders.push({
+            name: 'Client-ID',
+            value: cid
+        });
+    }
 	if (!replaced) {
 		details.requestHeaders.push({
 			name: 'Accept',
 			value: acceptHeader
 		});
 	}
+    if (token.length > 0) {
+        details.requestHeaders.push({
+            name: 'Authorization',
+            value: 'OAuth ' + token
+        })
+    }
     return { requestHeaders: details.requestHeaders };
 }, {
     urls: [
--- options.js
+++ options.js
@@ -2,19 +2,30 @@
 document.addEventListener('DOMContentLoaded', function () {
     chrome.storage.sync.get({
         cid: '',
-		version: 3
+		version: 0,
+        token: ''
     }, function (items) {
         document.getElementById('cid').value = items.cid;
+        document.getElementById('token').value = items.token;
         document.getElementById('version').value = items.version;
     });
 });
 document.getElementById('save').addEventListener('click', function () {
+    var version = document.getElementById('version').value;
     chrome.storage.sync.set({
         cid: document.getElementById('cid').value,
-		version: document.getElementById('version').value
+		version: version,
+        token: document.getElementById('token').value
     }, function () {
         var status = document.getElementById('status');
         status.textContent = 'Saved';
-        setTimeout(function () { status.textContent = '' }, 1000);
+        setTimeout(function () { status.textContent = '' }, 1000)
     });
+    chrome.extension.getBackgroundPage().cid = document.getElementById('cid').value;
+    if (version > 0) {
+        chrome.extension.getBackgroundPage().acceptHeader = 'application/vnd.twitchtv.v' + version + '+json';
+    } else {
+        chrome.extension.getBackgroundPage().acceptHeader = 'application/json';
+    }
+    chrome.extension.getBackgroundPage().token = document.getElementById('token').value;
 });
